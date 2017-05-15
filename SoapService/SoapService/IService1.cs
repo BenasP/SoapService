6a61c13db50cecdd3160f4c36efa2ca6db6316e6
Benediktas Prekeris <benediktas@adroiti.com>
2017-05-15 10:06:35 +0300

    Added default Soap service project

---
+++ SoapService/SoapService/IService1.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace SoapService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    [DataContract]
+    public class CompositeType
+    {
+        [DataMember]
+        public bool BoolValue { get; set; } = true;
+
+        [DataMember]
+        public string StringValue { get; set; } = "Hello ";
+    }
+}
---
+++ SoapService/SoapService/Service1.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SoapService
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return $"You entered: {value}";
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
