7899b4f3d97f05c33bb38665b35e53242349cdb9
Benediktas Prekeris <benediktas@adroiti.com>
2017-05-15 10:20:49 +0300

    Added Client and test code

---
+++ SoapService/Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Client.ServiceReference;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClient = new ServiceClient();
+
+            var result = myClient.GetData(1);
+        }
+    }
+}
